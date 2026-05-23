1b16e712c74f87fde8040f5e3de8539c91971a47
mahtabvariyani <mahtabvariyani@gmail.com>
2026-05-23 23:39:15 +0200

    add new lines of code

--- main.py
+++ main.py
@@ -30,13 +30,37 @@
 
 # string and Numbers
 
-print("You are going to a Restaurant and you ordered food ")
-food = int(input("How much did you pay for the food? "))
-tipBefore = int(input("How much tip?"))
-tip = tipBefore / 100
-actualTip = food * tip
+# print("You are going to a Restaurant and you ordered food ")
+# food = int(input("How much did you pay for the food? "))
+# tipBefore = int(input("How much tip?"))
+# tip = tipBefore / 100
+# actualTip = food * tip
 
-finalPrice = actualTip + food
-print(f'Food Amount: ${food}')
-print(f'Tip Amount: ${tipBefore}')
-print("$" + str(finalPrice))
+# finalPrice = actualTip + food
+# print(f'Food Amount: ${food}')
+# print(f'Tip Amount: ${tipBefore}')
+# print("$" + str(finalPrice))
+
+# #boolean
+# weather = input("what is the weather like?")
+
+# if weather == "rain ,snow , cloudy ,stormy":
+#     print("take an umbrella")
+# elif weather == "sunny and hot":
+#     print("wear sunglasses")
+# else:
+#     print("enjoy the day")
+
+# grade = int(input("what is your grade?"))
+
+# if grade >= 90:
+#     print("A")
+# elif grade >= 80:
+#     print("B")
+# elif grade >= 70:
+#     print("C")
+# elif grade >= 60:
+#     print("D")
+# else:
+#     print("F")
+    
